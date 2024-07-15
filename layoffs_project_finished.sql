91b423c20c1762bc237e142f3b4ef54b735cd653
isaacjlek <isaacjlek@gmail.com>
2024-07-15 19:24:22 -0400

    Add files via upload

---
+++ layoffs_project_finished.sql
@@ -0,0 +1,244 @@
+-- DATA CLEANING
+-- 1. Remove Duplicates
+-- 2. Standardize the Data
+-- 3. Null Values or Blank Values
+-- 4. Remove Unecessary Columns
+
+SET SQL_SAFE_UPDATES = 0;
+
+-- Creating staging table
+
+CREATE TABLE layoffs_staging
+LIKE layoffs;
+
+SELECT *
+FROM layoffs_staging;
+
+INSERT layoffs_staging
+SELECT *
+FROM layoffs;
+
+-- Duplicate removal
+
+-- Partitioned the duplicates together and indicated them through a row number greater than 1 
+
+WITH duplicate_cte AS
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, 
+total_laid_off, percentage_laid_off, `date`, stage,
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging
+)
+SELECT *
+FROM duplicate_cte
+WHERE row_num > 1;
+
+-- Created a second staging table equivalent to the first to get rid of the duplicates: CTE table can't be updated
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, 
+total_laid_off, percentage_laid_off, `date`, stage,
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging;
+
+DELETE
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging2;
+
+-- Standardizing Data
+
+-- Trimmed the white space in the company names
+
+SELECT company, TRIM(COMPANY)
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET company = TRIM(company);
+
+-- Checked for matching industries with contrasting names, and corrected them to have the same name
+
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY 1;
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry LIKE 'Crypto%';
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+-- Looked for and fixed errors in the country column
+
+SELECT DISTINCT country
+FROM layoffs_staging2
+ORDER BY 1;
+
+SELECT DISTINCT country, TRIM(TRAILING '.' FROM country)
+FROM layoffs_staging2
+ORDER BY 1;
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country)
+WHERE country LIKE 'United States%';
+
+-- To do time series exploratory data analysis and visualizations later the 'date' column needs to be changed from a 'TEXT' data type to a 'DATE'  
+
+SELECT `date`,
+STR_TO_DATE(`date`, '%m/%d/%Y')
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+-- Populated the blank or null values if the data was available
+
+-- Replaced the 'blank' values with 'NULLS'
+
+UPDATE layoffs_staging2
+SET industry = NULL
+WHERE industry = '';
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL;
+
+SELECT t1.industry, t2.industry
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL;
+
+SELECT *
+FROM layoffs_staging2;
+
+-- Deleted the rows that had 'NULLS' for both 'total_laid_off' and 'percentage_laid_off' because they would not contribute to the analysis
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+DELETE 
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT *
+FROM layoffs_staging2;
+
+-- Removed the 'row_num' column because it is not necessary
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
+
+
+-- Exploratory Data Analysis
+
+-- Found the companies that had a total business closure resulting in mass layoffs
+
+SELECT *
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1
+ORDER BY total_laid_off DESC;
+
+-- Listed the countries with the most layoffs in descending order
+
+SELECT country, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country 
+ORDER BY 2 DESC;
+
+-- Found the sum of all layoffs for each given year 
+
+SELECT YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY YEAR(`date`)
+ORDER BY 1 DESC;
+
+-- Listed the total layoffs for each month
+
+SELECT SUBSTRING(`date`, 1, 7) AS `MONTH`, SUM(total_laid_off)
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`, 1, 7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC;
+
+-- Displayed the rolling total of layoffs each month in ascending order by month
+
+WITH Rolling_Total AS
+(
+SELECT SUBSTRING(`date`, 1, 7) AS `MONTH`, SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`, 1, 7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC
+)
+SELECT `MONTH`, total_off,
+SUM(total_off) OVER(ORDER BY `MONTH`) AS rolling_total
+FROM Rolling_Total;
+
+-- Found the sum of each companies total layoffs in descending order by layoffs
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company 
+ORDER BY 2 DESC;
+
+-- Listed the top 5 companies with the most amount of layoffs in a given year
+
+WITH Company_Year (company, years, total_laid_off) AS
+(
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+), Company_Year_Rank AS
+(SELECT *, 
+DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS Ranking
+FROM Company_Year
+WHERE years IS NOT NULL
+)
+SELECT * 
+FROM Company_Year_Rank
+WHERE Ranking <= 5;
+
+
+
+
+
+
+
+
